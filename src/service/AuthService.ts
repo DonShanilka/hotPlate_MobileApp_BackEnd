ddb9e562366e27922fceab51f1b3ad045c275b6e
Shanilka800@# <shanilka799@gmail.com>
2025-03-25 22:12:41 +0530

    create UpdateUser in UserService

--- src/service/AuthService.ts
+++ src/service/AuthService.ts
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+
 const Auth = require('../model/AuthModel');
 import bcrypt from 'bcrypt/bcrypt'
 
@@ -12,9 +14,31 @@ export async function AddUser(authData) {
         const saved = await Auth.create(added);
         return saved;
     } catch (error) {
-        console.log("Error Save Auth in AuthService: ", error);
-
+        console.log("Error Save Auth in AuthService:  ", error);
     }
 }
 
+const UpdateUser = async (id, userData) => {
+    try {
+        console.log("Updating User in Service:", id, userData);
 
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            throw new Error("Invalid User ID");
+        }
+
+        const updateUser = await Auth.findByIdAndUpdate(
+            id,
+            {$set: userData},
+            {new: true, runValidators: true}
+        );
+
+        if (!updateUser) {
+            throw new Error("Customer not Found");
+        }
+
+        return updateUser;
+    } catch (error) {
+        console.error("Error in UserService update: ", error);
+        throw new Error("Failed to Update User");
+    }
+}
