96ef0632e1e530c6dc1da60f54f3c81a0139f050
Shanilka800@# <shanilka799@gmail.com>
2025-03-26 12:22:54 +0530

    fix update

--- src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-// @ts-ignore
-import authController from "../controllers/AuthController";
-
-export default function authRouters() {
-    router.post('/auth/saveAuth', authController.addAuth);
-    router.post('/auth/updateAuth', authController.updateAuth);
-}
