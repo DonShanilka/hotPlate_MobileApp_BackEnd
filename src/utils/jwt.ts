1b309c1fb964208bf4b17a4df4861d09054e2429
DonShanilka <shanilka799@gmail.com>
2026-07-11 21:20:54 +0530

    create jwt.ts

---
+++ src/utils/jwt.ts
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "food_delivery_secret";
+
+export const generateToken = (id: string) => {
+  return jwt.sign(
+    {
+      id,
+    },
+    JWT_SECRET,
+    {
+      expiresIn: "7d",
+    },
+  );
+};
