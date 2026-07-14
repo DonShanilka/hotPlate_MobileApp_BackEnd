c15b8a43e6e69ede6c9a43a8accbed8e064fe48c
DonShanilka <shanilka799@gmail.com>
2026-07-14 21:58:51 +0530

    create menu model

---
+++ src/modules/menu/menu.model.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+const menuSchema = new Schema(
+  {
+    restaurantId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Restaurant",
+      required: true,
+    },
+
+    name: {
+      type: String,
+      required: true,
+    },
+
+    description: String,
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    image: Buffer,
+
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model("Menu", menuSchema);
